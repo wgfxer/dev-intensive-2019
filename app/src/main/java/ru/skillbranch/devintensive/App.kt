c118bcab01611bc08fcf7d9481f282aae9786545
wgfxer <wgfxer@gmail.com>
2020-01-01 02:54:33 +0300

    hometask_4 initial commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive
+
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+
+    companion object {
+        private var instance: App? = null
+
+        fun applicationContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
